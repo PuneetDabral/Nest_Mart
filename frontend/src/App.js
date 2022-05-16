a936c100a0a0dcade5fed6a3fb7f0aab00e2315e
Puneet Dabral <puneetdabral2000@gmail.com>
2022-05-16 23:44:35 +0530

    create header,and  home component

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Component/Home/Header';
+import Home from './Component/Home/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Home />
+
+    
+    </div>
+  );
+}
+
+export default App;
