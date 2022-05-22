b03a5cad53d82fd4d57982a6c0585ae6190221c7
Puneet Dabral <puneetdabral2000@gmail.com>
2022-05-22 18:28:13 +0530

    create store and use redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Header from './Component/Home/Header';
 import Home from './Component/Home/Home';
+import WebFont from 'webfontloader';
+import { useEffect } from 'react';
+
 
 
 
 function App() {
+
+  
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+    
+  }, []);
+  
   return (
     <div className="App">
     <Header />
