d1f47d729f9435830c232790ebb11f2485241db4
Puneet Dabral <puneetdabral2000@gmail.com>
2022-06-20 02:26:06 +0530

    stripe not working on deploy

--- backend/controller/PaymentController.js
+++ backend/controller/PaymentController.js
@@ -2,8 +2,8 @@ const catchAsyncErrors = require("../middleware/catchAsyncError");
 
 
 
-const stripe = require("stripe")('sk_test_51KHZtYSDRLLSnc3wFje8mQwke2MnaoVFM6QUikpDVmzZx3NWogxrMGDilVTLLWs8vPKfK9Q3mopOjSrwdi5KLOPd00RooDI9zO');
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const stripe = require("stripe")('sk_test_51KHZtYSDRLLSnc3wFje8mQwke2MnaoVFM6QUikpDVmzZx3NWogxrMGDilVTLLWs8vPKfK9Q3mopOjSrwdi5KLOPd00RooDI9zO');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.Payment = catchAsyncErrors(async (req, res, next) => {
   const myPayment = await stripe.paymentIntents.create({
@@ -20,6 +20,6 @@ exports.Payment = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).json({ stripeApiKey: 'pk_test_51KHZtYSDRLLSnc3wlh28GmgCLAROIkjK5IyxgNAR7SYSLVIr0a4GnOo6HcPVJXsA5dw6ttj3AJulGbqAmZ09IDFu00UjGGORIs' });
-  // res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+  // res.status(200).json({ stripeApiKey: 'pk_test_51KHZtYSDRLLSnc3wlh28GmgCLAROIkjK5IyxgNAR7SYSLVIr0a4GnOo6HcPVJXsA5dw6ttj3AJulGbqAmZ09IDFu00UjGGORIs' });
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
 });
